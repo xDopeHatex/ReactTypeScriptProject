94ec0459057cd9eeec1c918165fa7a72974a063d
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-07-16 04:20:33 +0300

    feat: Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 function App() {
+  const yeah = async () => {
+    setTimeout(() => console.log(1), 0);
+    await Promise.resolve(2)
+      .then(console.log)
+      .catch((err) => console.log(err));
+  };
+
+  yeah();
+
   return <div className="app"></div>;
 }
 
